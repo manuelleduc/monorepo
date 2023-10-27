da3d837638061e34231c2af37af51351420bd872
Manuel Leduc <manuel.leduc@xwiki.com>
2023-10-27 10:56:25 +0200

    After implementation the back-end module (server + test)

--- packages/back-end/__tests__/back-end.test.js
+++ packages/back-end/__tests__/back-end.test.js
@@ -1,7 +1,19 @@
-'use strict';
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../lib/back-end'); // Import the Express app
 
-const backEnd = require('..');
-const assert = require('assert').strict;
+// using request server as chaiHttp;
+chai.use(chaiHttp);
+const expect = chai.expect;
 
-assert.strictEqual(backEnd(), 'Hello from backEnd');
-console.info('backEnd tests passed');
+describe('Backend Server', () => {
+  it('should log a request and process payment', (done) => {
+    chai.request(app)
+      .get('/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.text).to.equal('Backend Server: Running!');
+        done();
+      });
+  });
+});
--- packages/back-end/lib/back-end.js
+++ packages/back-end/lib/back-end.js
@@ -1,7 +1,23 @@
-'use strict';
+const express = require('express');
+const loggingService = require('logging');
+const paymentService = require('payment');
 
-module.exports = backEnd;
+const app = express();
 
-function backEnd() {
-  return 'Hello from backEnd';
-}
+app.get('/', (req, res) => {
+  loggingService.info('Backend server received a request');
+
+  const paymentResult = paymentService.makePayment(100);
+  loggingService.info('Payment result:', paymentResult);
+
+  res.send('Backend Server: Running!');
+
+});
+
+const port = 3000;
+const server = app.listen(port, () => {
+  console.log(`Backend server is running on port ${port}`)
+});
+
+module.exports = server;
+
